a2c87bd40635ffb9fa1caf1f091bced14400eaa8
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-12-08 23:49:51 +0100

    Front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,6 @@
 class VisitorsController < ApplicationController
 
 		def new
-			@owner = Owner.new
-			flash.now[:notice] = "Heeej!"
-			flash.now[:alert] = "Fyller år snart!"
-			
+			@owner = Owner.new		
 		end
 	end
